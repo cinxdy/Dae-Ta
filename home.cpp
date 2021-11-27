66dbc362b8ecc754ce7356ec36a5008aef31e01c
cinxdy <cinxdys2@gmail.com>
2021-11-27 05:28:11 -0500

    Update home

--- home.cpp
+++ home.cpp
@@ -2,23 +2,33 @@
 #include "ui_home.h"
 
 #include <QString>
+#include <QMessageBox>
+#include<QDebug>
+
 
 static QStringList qStrListOptionTable_Headers = {"TableNo","Floor"};
 
 int m_listCount;
-int locationX=0;
-int locationY=0;
-int stateLocation; // 0:home -1:moving n:tableN
+int locationX;
+int locationY;
+Location stateLocation; // 0:home -1:moving n:tableN
+Location destination;
 Home::Home(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::Home)
 {
     ui->setupUi(this);
-    stateLocation=0;
+    locationX=170;
+    locationY=200;
+    stateLocation=HOME;
+    destination=HOME;
+    
     if(stateLocation==0) ui->btnOrderOrServe->setText("Serve");
     else ui->btnOrderOrServe->setText("Order");
 
-    ui->tableServingOrder->setRowCount(10);
+
+    // Serving Table
+    // ui->tableServingOrder->setRowCount(10);
     ui->tableServingOrder->setColumnCount(2);
     ui->tableServingOrder->setColumnWidth(0,120);
     ui->tableServingOrder->setColumnWidth(1,60);
@@ -37,7 +47,8 @@ Home::Home(QWidget *parent) :
     connect(ui->btnTable5, SIGNAL(clicked()), this, SLOT(addTable5()));
     connect(ui->btnTable6, SIGNAL(clicked()), this, SLOT(addTable6()));
 
-
+    // Serving Button
+    connect(ui->btnOrderOrServe,SIGNAL(clicked()),this,SLOT(servingStart()));
 }
 
 Home::~Home()
@@ -53,7 +64,7 @@ void Home::addTable5(){addTable(5);}
 void Home::addTable6(){addTable(6);}
 
 void Home::addTable(int num){
-
+    ui->tableServingOrder->insertRow(m_listCount);
     ui->tableServingOrder->setItem(m_listCount,0,new QTableWidgetItem(QString("Table%1").arg(num)));
     ui->tableServingOrder->setItem(m_listCount,1, new QTableWidgetItem("1"));
     m_listCount++;
@@ -61,4 +72,41 @@ void Home::addTable(int num){
 
 //    retv=msgInputBox.warning(this, "Confirm","Data Input",QMessageBox::Cancel|QMessageBox::Ok);
 
+}
+
+void Home::servingStart(){
+
+    while(ui->tableServingOrder->rowCount()!=0){
+        if(ui->tableServingOrder->item(0,0)->text()=="Table1") destination=TABLE1;
+        else if(ui->tableServingOrder->item(0,0)->text()=="Table2") destination=TABLE2;
+        else if(ui->tableServingOrder->item(0,0)->text()=="Table3") destination=TABLE3;
+        else if(ui->tableServingOrder->item(0,0)->text()=="Table4") destination=TABLE4;
+        else if(ui->tableServingOrder->item(0,0)->text()=="Table5") destination=TABLE5;
+        else if(ui->tableServingOrder->item(0,0)->text()=="Table6") destination=TABLE6;
+
+        
+        stateLocation=MOVING; // moving state
+        QTextStream(stdout)<<destination;
+
+        int mvResult = goToTable(destination);
+        if(mvResult) {
+            stateLocation = destination;
+
+            do{
+                int retv=msgClearBox.warning(this, "Confirm","Did you get your plates?", QMessageBox::Cancel|QMessageBox::Ok);
+                if(retv==QMessageBox::Ok) break;
+            while(true);
+
+        }
+        
+
+        ui->tableServingOrder->removeRow(0);
+        m_listCount--;
+    }
+}
+
+int Home:goToTable(Location dest){
+    
+    
+    return 1;
 }
--- home.h
+++ home.h
@@ -2,6 +2,7 @@
 #define HOME_H
 
 #include <QMainWindow>
+enum Location {MOVING=-1,HOME, TABLE1,TABLE2,TABLE3,TABLE4,TABLE5,TABLE6}; // 0:home -1:moving n:tableN
 
 namespace Ui {
 class Home;
@@ -9,12 +10,13 @@ class Home;
 
 class Home : public QMainWindow
 {
+
     Q_OBJECT
 
 public:
     explicit Home(QWidget *parent = nullptr);
     ~Home();
-
+    
 public slots:
     void addTable(int);
     void addTable1();
@@ -23,6 +25,9 @@ public slots:
     void addTable4();
     void addTable5();
     void addTable6();
+
+    void servingStart();
+    int goToTable(int);
 
 private:
     Ui::Home *ui;
